61cb7fecc7abf47f05f5bbb3728221a2a5800328
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-06-05 19:08:21 -0600

    Agregar archivos de proyecto.

---
+++ CRUD/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Empleado
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [NotNull]
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+    }
+}
